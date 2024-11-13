bd09894788e84f9377c3bb3284e3eccea74866bb
idityaGE <am44910606@gmail.com>
2024-11-13 23:53:17 +0530

    theme animation is added

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,25 +1,43 @@
 import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
-import { useTheme } from "@/components/theme-provider"
-import { useState } from "react"
+import { useTheme } from "./theme-provider"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
-  const [isDark, setIsDark] = useState(true)
+  const { theme, setTheme } = useTheme()
+
+  const switchTheme = () => {
+    switch (theme) {
+      case "light":
+        setTheme("dark");
+        break;
+      case "dark":
+        setTheme("light");
+        break;
+      default:
+        break;
+    }
+  };
+
+  const toggleTheme = () => {
+    //@ts-ignore
+    if (!document.startViewTransition) switchTheme();
+
+    //@ts-ignore
+    document.startViewTransition(switchTheme);
+  };
 
 
   return (
     <Button
-      variant="outline"
+      onClick={toggleTheme}
+      variant="ghost"
       size="icon"
-      onClick={() => {
-        setIsDark(!isDark)
-        setTheme(isDark ? "light" : "dark")
-      }}
+      className="rounded-full"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
     </Button>
   )
 }
