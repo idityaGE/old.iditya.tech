303efbab7fddb02d7bafa7d9aca88b9bf3eb2ede
idityaGE <am44910606@gmail.com>
2024-11-14 11:39:21 +0530

    markdown is working but table is not working

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,10 +1,41 @@
 // import { useLocation } from "react-router-dom";
 // import React from 'react'
 
-const ProjectPage = () => {
-  return (
-    <div>Working On it, comming soon...</div>
-  )
+import React from 'react';
+import MarkdownRenderer from '@/components/md/MarkdownRender';
+
+const MyPage: React.FC = () => {
+  const markdownContent = `
+# TypeScript Markdown Example
+
+## Table Example
+| Name | Age | City |
+|------|-----|------|
+| John | 25  | NYC  |
+| Jane | 30  | LA   |
+
+## Video Example
+youtube: https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
+## Code Example
+\`\`\`typescript
+interface User {
+  name: string;
+  age: number;
 }
 
-export default ProjectPage
+const user: User = {
+  name: "John",
+  age: 25
+};
+\`\`\`
+  `;
+
+  return (
+    <div className=''>
+      <MarkdownRenderer content={markdownContent} />
+    </div>
+  );
+};
+
+export default MyPage;
